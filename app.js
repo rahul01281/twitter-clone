5d55a0d022d25f5e40115e8e5b38d7d1a1c32cf0
Rahul Rinwa <rahul.rinwah@gmail.com>
2021-05-25 03:51:48 +0530

    initialialized pug templates

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const server = app.listen(PORT, () =>
   console.log(`server listening on port ${PORT}`)
 )
 
+app.set('view engine', 'pug') //setting the view engine to pug
+app.set('views', 'views') //go to folder called views
+
 app.get('/', (req, res, next) => {
-  res.status(200).send('hello')
+  res.status(200).render('home')
 })
