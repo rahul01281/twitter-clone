62cc343e3e909dde10b7bb176bef47cf6fae3171
Rahul Rinwa <rahul.rinwah@gmail.com>
2021-05-25 12:12:15 +0530

    checking if user already exists

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const router = express.Router()
 const bodyParser = require('body-parser')
+const User = require('../models/UserSchema')
 
 app.set('view engine', 'pug') //setting the view engine to pug
 app.set('views', 'views') //go to folder called
@@ -15,11 +16,9 @@ router.get('/', (req, res, next) => {
   res.status(200).render('register', payload)
 })
 
-router.post('/', (req, res, next) => {
-  console.log(req.body)
+router.post('/', async (req, res, next) => {
   payload = {
     title: 'Register',
-    errorMessage: 'Make sure each field has valid value.',
   }
 
   var firstName = req.body.firstName.trim()
@@ -29,10 +28,31 @@ router.post('/', (req, res, next) => {
   var password = req.body.password
 
   if (firstName && lastName && username && email && password) {
-  } else {
-  }
+    var user = await User.findOne({
+      $or: [{ username: username }, { email: email }],
+    }).catch((error) => {
+      console.log(error)
+      payload.errorMessage = 'Something went wrong.'
+      res.status(200).render('register', payload)
+    })
 
-  res.status(200).render('register', payload)
+    if (user === null) {
+      //no user found
+    } else {
+      //user found
+      if (email === user.email) {
+        payload.errorMessage = 'Email already in user.'
+      } else {
+        payload.errorMessage = 'Username already in user.'
+      }
+      res.status(200).render('register', payload)
+    }
+
+    console.log('hello')
+  } else {
+    payload.errorMessage = 'Make sure each field has valid value.'
+    res.status(200).render('register', payload)
+  }
 })
 
 module.exports = router
