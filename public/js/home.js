0ef426b8b3816e0acaf0fad1ec1f80553f585ea2
Rahul Rinwa <rahul.rinwah@gmail.com>
2021-05-26 18:06:32 +0530

    outputting posts on page

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,18 @@
 $(document).ready(() => {
   $.get('/api/posts', (posts, status, xhr) => {
-    console.log(posts)
+    outputPosts(posts, $('.postsContainer'))
   })
 })
+
+function outputPosts(posts, container) {
+  container.html('')
+
+  posts.forEach((post) => {
+    var html = createPostHtml(post)
+    container.append(html)
+  })
+
+  if (posts.length === 0) {
+    container.append("<span class='noResults'>Nothing to show :(</span>")
+  }
+}
