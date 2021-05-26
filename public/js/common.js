18d3f1a1d517feb1f54a7c79f82d703682c6e6cc
Rahul Rinwa <rahul.rinwah@gmail.com>
2021-05-26 13:41:13 +0530

    enabling submit button when user types

--- public/js/common.js
+++ public/js/common.js
@@ -1,5 +1,15 @@
 $('#postTextarea').keyup((e) => {
   var textbox = $(e.target)
   var value = textbox.val().trim() //getting entered text in textbox
-  console.log(value)
+
+  var submitButton = $('#submitPostButton')
+
+  if (submitButton.length === 0) return alert('no submit button found')
+
+  if (value == '') {
+    submitButton.prop('disabled', true) //if there is nothing in textarea, then set button to diabled
+    return
+  }
+
+  submitButton.prop('disabled', false)
 })
