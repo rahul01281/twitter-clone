1e0e51a40bbc35bd240cddc184f7e0150a5d8d6e
Rahul Rinwa <rahul.rinwah@gmail.com>
2021-05-26 18:21:19 +0530

    calculating timestamp of posts

--- public/js/common.js
+++ public/js/common.js
@@ -32,6 +32,33 @@ $('#submitPostButton').click((e) => {
   })
 })
 
+function timeDifference(current, previous) {
+  var msPerMinute = 60 * 1000
+  var msPerHour = msPerMinute * 60
+  var msPerDay = msPerHour * 24
+  var msPerMonth = msPerDay * 30
+  var msPerYear = msPerDay * 365
+
+  var elapsed = current - previous
+
+  if (elapsed < msPerMinute) {
+    if (elapsed / 1000 < 30) {
+      return 'Just mow'
+    }
+    return Math.round(elapsed / 1000) + ' seconds ago'
+  } else if (elapsed < msPerHour) {
+    return Math.round(elapsed / msPerMinute) + ' minutes ago'
+  } else if (elapsed < msPerDay) {
+    return Math.round(elapsed / msPerHour) + ' hours ago'
+  } else if (elapsed < msPerMonth) {
+    return Math.round(elapsed / msPerDay) + ' days ago'
+  } else if (elapsed < msPerYear) {
+    return Math.round(elapsed / msPerMonth) + ' months ago'
+  } else {
+    return Math.round(elapsed / msPerYear) + ' years ago'
+  }
+}
+
 function createPostHtml(postData) {
   var postedBy = postData.postedBy
 
@@ -40,7 +67,7 @@ function createPostHtml(postData) {
   }
 
   var displayName = postedBy.firstName + ' ' + postedBy.lastName
-  var timestamp = postData.createdAt
+  var timestamp = timeDifference(new Date(), new Date(postData.createdAt))
 
   return `<div class='post'>
             <div class='mainContentContainer'>
