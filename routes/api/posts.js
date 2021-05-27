ceef1c797fd3070f12448844b43fc89b300a3153
Rahul Rinwa <rahul.rinwah@gmail.com>
2021-05-27 10:18:30 +0530

    populating retweet's postedBy field

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -12,7 +12,10 @@ router.get('/', (req, res, next) => {
     .populate('postedBy')
     .populate('retweetData')
     .sort({ createdAt: -1 })
-    .then((posts) => res.status(200).send(posts))
+    .then(async (posts) => {
+      posts = await User.populate(posts, { path: 'retweetData.postedBy' })
+      res.status(200).send(posts)
+    })
     .catch((error) => {
       console.log(error)
       res.sendStatus(400)
