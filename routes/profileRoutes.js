079ea9a0005f4b8c8df75c6d7cbda9ead4b608e7
Rahul Rinwa <rahul.rinwah@gmail.com>
2021-05-29 10:23:15 +0530

    accessing profile page by id

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -23,10 +23,14 @@ router.get('/:username', async (req, res, next) => {
 async function getPayload(username, userLoggedIn) {
   var user = await User.findOne({ username: username })
   if (user === null) {
-    return {
-      pageTitle: 'User not found',
-      userLoggedIn: userLoggedIn,
-      userLoggedInJs: JSON.stringify(userLoggedIn),
+    user = await User.findById(username)
+
+    if (user === null) {
+      return {
+        pageTitle: 'User not found',
+        userLoggedIn: userLoggedIn,
+        userLoggedInJs: JSON.stringify(userLoggedIn),
+      }
     }
   }
 
