819b25f22a114ac1dcb4418f772ab1784c4d0ffd
Rahul Rinwa <rahul.rinwah@gmail.com>
2021-05-29 10:12:32 +0530

    accessing profile page by username

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const router = express.Router()
+const User = require('../models/UserSchema')
 
 router.get('/', (req, res, next) => {
   var payload = {
@@ -12,5 +13,29 @@ router.get('/', (req, res, next) => {
 
   res.status(200).render('profilePage', payload)
 })
+
+router.get('/:username', async (req, res, next) => {
+  var payload = await getPayload(req.params.username, req.session.user)
+
+  res.status(200).render('profilePage', payload)
+})
+
+async function getPayload(username, userLoggedIn) {
+  var user = await User.findOne({ username: username })
+  if (user === null) {
+    return {
+      pageTitle: 'User not found',
+      userLoggedIn: userLoggedIn,
+      userLoggedInJs: JSON.stringify(userLoggedIn),
+    }
+  }
+
+  return {
+    pageTitle: user.username,
+    userLoggedIn: userLoggedIn,
+    userLoggedInJs: JSON.stringify(userLoggedIn),
+    profileUser: user,
+  }
+}
 
 module.exports = router
