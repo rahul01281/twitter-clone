203268c7d5dc4668dd2207f863c865042781bad4
Rahul Rinwa <rahul.rinwah@gmail.com>
2021-06-05 12:43:18 +0530

    getting tha chat data for the page

--- routes/messagesRoutes.js
+++ routes/messagesRoutes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const router = express.Router()
 const User = require('../models/UserSchema')
+const Chat = require('../models/ChatSchema')
 
 router.get('/', (req, res, next) => {
   var payload = {
@@ -23,11 +24,24 @@ router.get('/new', (req, res, next) => {
   res.status(200).render('newMessage', payload)
 })
 
-router.get('/:chatId', (req, res, next) => {
+router.get('/:chatId', async (req, res, next) => {
+  var userId = req.session.user._id
+  var chatId = req.params.chatId
+
+  var chat = await Chat.findOne({
+    _id: chatId,
+    users: { $elemMatch: { $eq: userId } },
+  }).populate('users')
+
+  if (chat == null) {
+    // check if chat id is really user id
+  }
+
   var payload = {
     pageTitle: 'Chat',
     userLoggedIn: req.session.user,
     userLoggedInJs: JSON.stringify(req.session.user),
+    chat: chat,
   }
 
   res.status(200).render('chatPage', payload)
