a5c44fb13a84ea754e1fd9c57dc511fa3fafad2d
Rahul Rinwa <rahul.rinwah@gmail.com>
2021-06-07 13:50:06 +0530

    hiding the typing dots when user stops typing

--- public/js/chatPage.js
+++ public/js/chatPage.js
@@ -1,7 +1,15 @@
+var typing = false
+var lastTypingTime
+
 $(document).ready(() => {
   socket.emit('join room', chatId) //it will emit an event to the server called "join room" and will pass in the chatId
+
   socket.on('typing', () => {
     $('.typingDots').show()
+  })
+
+  socket.on('stop typing', () => {
+    $('.typingDots').hide()
   })
 
   $.get(`/api/chats/${chatId}`, (data) => {
@@ -56,7 +64,25 @@ $('.inputTextbox').keydown((e) => {
 })
 
 function updateTyping() {
-  socket.emit('typing', chatId) //when we're typing, it will send a notification that we're typing (cont. on app.js line 80)
+  if (!connected) return
+
+  if (!typing) {
+    typing = true
+    socket.emit('typing', chatId) //when we're typing, it will send a notification that we're typing (cont. on app.js line 80)
+  }
+
+  lastTypingTime = new Date().getTime()
+  var timerLength = 3000
+
+  setTimeout(() => {
+    var timeNow = new Date().getTime()
+    var timeDiff = (timeNow = lastTypingTime)
+
+    if (timeDiff >= timerLength && typing) {
+      socket.emit('stop typing', chatId)
+      typing = false
+    }
+  }, timerLength)
 }
 
 function addMessagesHtmlToPage(html) {
