bb429420e4e95bb9b79311400f80802bc43e514d
Rahul Rinwa <rahul.rinwah@gmail.com>
2021-06-08 16:16:42 +0530

    creating the notifications html

--- public/js/notificationsPage.js
+++ public/js/notificationsPage.js
@@ -1,5 +1,29 @@
 $(document).ready(() => {
-  $.get('/api/notifications', (data) => {
-    console.log(data)
+  $.get('/api/notifications', (notifications) => {
+    outputNotificationsList(notifications, $('.resultsContainer'))
   })
 })
+
+function outputNotificationsList(notifications, container) {
+  notifications.forEach((notification) => {
+    var html = createNotificationHtml(notification)
+    container.append(html)
+  })
+
+  if (notifications.length == 0) {
+    container.append("<span class='noResults'>You have no notifications</span>")
+  }
+}
+
+function createNotificationHtml(notification) {
+  var userFrom = notification.userFrom
+
+  return `<a href='#' class='resultListItem notification'>
+                <div class='resultsImageContainer'>
+                    <img src='${userFrom.profilePic}'>
+                </div>
+                 <div class='resultsDetailsContainer ellipsis'>
+                    <span class='ellipsis'>this is the text</span>
+                </div>
+            </a>`
+}
