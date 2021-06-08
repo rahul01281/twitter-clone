06e37ded03519d2c5c804dd7f8e23f4ed4833e47
Rahul Rinwa <rahul.rinwah@gmail.com>
2021-06-08 16:22:37 +0530

    creating the notification text

--- public/js/notificationsPage.js
+++ public/js/notificationsPage.js
@@ -17,13 +17,37 @@ function outputNotificationsList(notifications, container) {
 
 function createNotificationHtml(notification) {
   var userFrom = notification.userFrom
+  var text = getNotificationText(notification)
 
   return `<a href='#' class='resultListItem notification'>
                 <div class='resultsImageContainer'>
                     <img src='${userFrom.profilePic}'>
                 </div>
                  <div class='resultsDetailsContainer ellipsis'>
-                    <span class='ellipsis'>this is the text</span>
+                    <span class='ellipsis'>${text}</span>
                 </div>
             </a>`
+}
+
+function getNotificationText(notification) {
+  var userFrom = notification.userFrom
+
+  if (!userFrom.firstName || !userFrom.lastName)
+    return alert('userFrom data not populated')
+
+  var userFromName = `${userFrom.firstName} ${userFrom.lastName}`
+
+  var text
+
+  if (notification.notificationType == 'retweet') {
+    text = `${userFromName} retweeted your post`
+  } else if (notification.notificationType == 'postLike') {
+    text = `${userFromName} liked your post`
+  } else if (notification.notificationType == 'reply') {
+    text = `${userFromName} replied to your post`
+  } else if (notification.notificationType == 'follow') {
+    text = `${userFromName} has started following you`
+  }
+
+  return `<span class='ellipsis'>${text}</span>`
 }
